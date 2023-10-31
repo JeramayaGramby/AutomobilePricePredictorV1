f31a78b65b06f126000d0f5b284875de9bc4d732
Jeramaya Gramby <jeramayagramby@gmail.com>
2023-10-30 23:59:25 -0400

    Fix compile error

--- Program.cs
+++ Program.cs
@@ -1,64 +1,72 @@
-﻿ 
-using Microsoft.VisualBasic;
-using MLTrained_AutomobilePricePredictor;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using MLTrained_AutomobilePricePredictor;
 
-// Create single instance of sample data from first line of dataset for model input
-// The "F" stays after the numeric value
-// Make the user input equal the values inside the struct
-// Change the variables to equal the input variables
-// Put everything into a class with two separate functions
-// One for handling user input
-// One for displaying user input
+namespace modelExecution
+{ 
 
-
-public class ModelConsumption
-{
-    static void Main(string[] args)
+    public class ModelConsumption
     {
-        try
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter the model year of the vehicle:");
-            Single year = Convert.ToSingle(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Enter the mileage of the vehicle:");
-            Single mileage = Convert.ToSingle(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Enter the vehicle manufacturer");
-            string make = Convert.ToString(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Enter the vehicle model:");
-            string model = Convert.ToString(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Enter the city where the vehicle is located:");
-            string city = Convert.ToString(Console.ReadLine());
-            Console.ReadKey();
-            Console.WriteLine("Enter the state of where the vehicle is located (Example: TX, PA):");
-            string state = Convert.ToString(Console.ReadLine());
-            Console.ReadKey();
-            // Put something in here that constructs an object for Model.predict()
-            // Log that to the console
-            //
+            try
+            {
+                Console.WriteLine("Enter the model year of the vehicle:");
+                Single year = Convert.ToSingle(Console.ReadLine());
+                Console.ReadKey();
+                
+                Console.WriteLine("Enter the mileage of the vehicle:");
+                Single mileage = Convert.ToSingle(Console.ReadLine());
+                Console.ReadKey();
+                
+                Console.WriteLine("Enter the vehicle manufacturer");
+                string make = Convert.ToString(Console.ReadLine());
+                Console.ReadKey();
+                
+                Console.WriteLine("Enter the vehicle model:");
+                string model = Convert.ToString(Console.ReadLine());
+                Console.ReadKey();
+                
+                Console.WriteLine("Enter the city where the vehicle is located:");
+                string city = Convert.ToString(Console.ReadLine());
+                Console.ReadKey();
+                
+                Console.WriteLine("Enter the state of where the vehicle is located (Example: TX, PA):");
+                string state = Convert.ToString(Console.ReadLine());
+                Console.ReadKey();
+               
 
-        }
-        
-        // The two most likely exceptions to be thrown are NullReferenceException and ArgumentException 
 
-        catch (NullReferenceException e)
-        {
-            Console.WriteLine("Something went wrong. Please restart the predictor.");
-            Console.WriteLine(e);
-        }
-        catch (ArgumentException e)
-        {
-            Console.WriteLine("Something went wrong. Please restart the predictor.");
-            Console.WriteLine(e);
-        }
+                MLModel2.ModelInput inputData = new MLModel2.ModelInput()
+                {
+                    Year = year,
+                    Mileage = mileage,
+                    City = city,
+                    State = state,
+                    Make = make,
+                    Model = model,
+                };
 
+                var predictionResult = MLModel2.Predict(inputData);
+                
+                Console.WriteLine("The estimated market value for a $"{year}" $"{make}" $"{model}" with " +
+                    "$"{mileage}" miles in the $"{city}" area is $$"{predictionResult}");
+
+            }
+
+            // The two most likely exceptions to be thrown are NullReferenceException and ArgumentException. 
+
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Something went wrong. Please restart the program.");
+                Console.WriteLine(e);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Something went wrong. Please restart the program.");
+                Console.WriteLine(e);
+            }
+            
+        }
     }
-}
         
-
+}
     
